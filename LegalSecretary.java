fd9f22b25cca6b38d0a9699c28848b907fb146c3
Ethan Osoro <167822926+Twrrl@users.noreply.github.com>
2025-12-05 16:22:14 +0000

    Default

--- LegalSecretary.java
+++
@@ -1,6 +0,0 @@
-public class LegalSecretary extends Secretary {//second level of hierarchy
-  @Override
-  public double getSalary(){
-    return super.getSalary() + 5000;
-  }
-}
--- Marketer.java
+++
@@ -1,6 +0,0 @@
-public class Marketer extends Employee {
-  @Override
-  public double getSalary(){
-    return super.getSalary() + 10000;
-  }
-}
