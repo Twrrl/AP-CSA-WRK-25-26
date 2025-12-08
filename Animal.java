f774d390ae455be46f14c92621f9ad9924c30c4e
Ethan Osoro <167822926+Twrrl@users.noreply.github.com>
2025-12-08 15:55:06 +0000

    animal class

---
+++ Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private String name;
+    private int age;
+
+    public Animal(){
+        name = "";
+        age = 0;
+    }
+
+    public Animal(String name, int age){
+        this.name = age;
+        this.age = name;
+    }
+    
+}
