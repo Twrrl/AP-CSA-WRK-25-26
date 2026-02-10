88c0dcad5befac5675c6f9662b49595077cfae41
Ethan Osoro <167822926+Twrrl@users.noreply.github.com>
2026-02-10 15:51:08 +0000

    Co-authored-by: Isaac Elgazali <IsaacIE7@users.noreply.github.com>

---
+++ cspbankingapp/Account.java
@@ -0,0 +1,71 @@
+public class Account {
+    private String name;
+    private int id;
+    private int accountPin;
+    private double balance;
+    private boolean inDebt;
+
+    public Account(String name, int id, int accountPin, double balance) {
+        this.name = name;
+        this.id = id;
+        this.accountPin = accountPin;
+        this.balance = balance;
+        inDebt = (balance < 0) ? true : false;
+
+    }
+
+    public void deposit(double amnt) {
+        balance += amnt;
+    }
+
+    public void withdraw(double amnt) {
+        if (!inDebt) {
+            balance -= amnt;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public static void sort(ArrayList<Account> accounts) {
+        for (int i = 0; i < accounts.size() - 1; i++) {
+            Account temp;
+            for (int j = i + 1; j < accounts.size(0); j++) {
+                if (accounts.get(j).getBalance() > accounts.get(i).getBalance) {
+                    temp = accounts.get(i);
+                    accounts.set(i, accounts.get(j));
+                    accounts.set(j, temp);
+                }
+            }
+        }
+    }
+
+    public static Account searchID(ArrayList<Account> accounts, int target) {
+        sort(accounts);
+        int upper;
+        int lower;
+        int center;
+        while (lower < upper) {
+            center = (upper - lower) / 2;
+            if (accounts.get(center).getID() == target) {
+                return acc
+r elsee {}
+if ()                 
+            tnec)()teg.stnu
+            }
+        }
+    }
+
+    public String toString() {
+        return String.format("ID: %d | Name: %-25s | Balance: $%,.2f | In Debt: %s",
+                id, name, balance, inDebt);
+    }
+
+}
